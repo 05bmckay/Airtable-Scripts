89fa1a7028aeedd7a65f57f6fac02f2a9ef5677f
Carter McKay <45646585+05bmckay@users.noreply.github.com>
2022-04-19 13:02:43 -0700

    Update Multi-Record-Updater.js

--- Multi-Record-Updater.js
+++ Multi-Record-Updater.js
@@ -1,8 +1,11 @@
-let record_ids = input.config().record_ids;// You will need to set a variable for this, make its value the output of your "Find Record" action.
-let table = base.getTable("TABLE_NAME"); // Replace with your table name.
-for (let record of records_ids) {
-    await table.updateRecordAsync(record, {
+let config = input.config()
+let record_ids = config.record_ids;// You will need to set a variable for this, make its value the output of your "Find Record" action.
+let table = base.getTable("TABLE"); // Replace with your table name.
+
+for (let i = 0; i < record_ids.length; i++) {
+  await table.updateRecordAsync(record_ids[i], {
     "FIELD_NAME":"VALUE", //Replace "FIELD_NAME" with the field for the found records you want to update, and replace "VALUE" with the value you want populated.
     })
 }
+
 //This is a pretty simple script but it does good work.
